ba47c7ab31170e219ee66fe2734e407036d66860
duck <jungmin5.hur@gmail.com>
2020-01-06 22:40:25 +0900

    refactoring

--- leetcode/src/ThreeSum.java
+++ leetcode/src/ThreeSum.java
@@ -3,16 +3,14 @@ import java.util.*;
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> retVal = new HashSet<>();
-        Map<Integer, List<Integer>> dic = new HashMap<>();
+        Map<Integer, Integer> dic = new HashMap<>();
 
         int tot = nums.length;
 
         Arrays.sort(nums);
 
-        for(int i=0; i<tot; i++) {
-            List<Integer> index = dic.getOrDefault(nums[i], new ArrayList<>());
-            index.add(i);
-            dic.put(nums[i], index);
+        for(int num : nums) {
+            dic.put(num, dic.getOrDefault(num,0)+1);
         }
 
         for(int i=0; i<tot; i++) {
@@ -20,33 +18,34 @@ public class ThreeSum {
             if(i > 0 && nums[i]==nums[i-1]) continue;
 
             for(int j=i+1; j<tot; j++) {
-                if(nums[j]==nums[j-1] && j!=i+1) continue;
+                if(nums[j]==nums[j-1] && j>i+1) continue;
 
-                int thirdNum = (nums[i]+nums[j]) * (-1);
-                if(!availableMember(dic, nums[i], nums[j], thirdNum)) continue;
+                int n1=nums[i], n2=nums[j];
+
+                int n3 = (n1 + n2) * (-1);
+                if(!availableMember(dic, n1, n2, n3)) continue;
 
                 // sort하는 것보다 직접 순서를 지정하면 50ms정도 빠름
-                List<Integer> zeroNums;
-                if(thirdNum <= nums[i]) {
-                    zeroNums = Arrays.asList(thirdNum, nums[i], nums[j]);
-                } else if(thirdNum <= nums[j]) {
-                    zeroNums = Arrays.asList(nums[i], thirdNum, nums[j] );
+                List<Integer> newList;
+                if(n3 <= n1) {
+                    newList = Arrays.asList(n3, n1, n2);
+                } else if(n3 <= n2) {
+                    newList = Arrays.asList(n1, n3, n2 );
                 } else {
-                    zeroNums = Arrays.asList(nums[i], nums[j], thirdNum);
+                    newList = Arrays.asList(n1, n2, n3);
                 }
 
-                retVal.add(zeroNums);
+                retVal.add(newList);
             }
         }
         return new ArrayList(retVal);
     }
 
     //if the number used before, the number couldn't be member of sum.
-    private boolean availableMember(Map<Integer, List<Integer>> dic, int n1, int n2, int n3) {
+    private boolean availableMember(Map<Integer, Integer> dic, int n1, int n2, int n3) {
         //n1,n2 couldn't duplicated and always exist in dic
-        List<Integer> n3List = dic.get(n3);
-        if(n3List == null) return false;
-        int n3ListSize = n3List.size();
+        int n3ListSize = dic.getOrDefault(n3, 0);
+        if(n3ListSize == 0) return false;
         if(n3ListSize >= 3) return true;
 
         if(n3 == n1 && n3 != n2) {
