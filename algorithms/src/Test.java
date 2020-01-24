6596db38de440c465d0c6564eb2c4d32c39f326b
duck <jungmin5.hur@gmail.com>
2020-01-24 23:32:08 +0900

    .

--- algorithms/src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String args[]) {
-        int a = 100;
-        while(a > 0) {
-            a = a/10;
-            System.out.println(a);
-        }
-    }
-}
