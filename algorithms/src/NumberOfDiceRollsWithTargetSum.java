2544ba5e748354fe3d98000e688d07e776ca6d6a
duck <jungmin5.hur@gmail.com>
2020-01-27 15:43:57 +0900

    ,

---
+++ algorithms/src/NumberOfDiceRollsWithTargetSum.java
@@ -0,0 +1,15 @@
+/**
+ * 1155. Number of Dice Rolls With Target Sum
+ * https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/
+ */
+public class NumberOfDiceRollsWithTargetSum {
+
+    public static void main(String args[]) {
+        System.out.println(numRollsToTarget(1, 6, 3));
+    }
+
+    public static int numRollsToTarget(int d, int f, int target) {
+
+        return 0;
+    }
+}
