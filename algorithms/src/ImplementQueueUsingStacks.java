ba42728844b715bc6cee002d92633424ee5d5c85
duck <jungmin5.hur@gmail.com>
2020-01-28 20:54:24 +0900

    .

--- algorithms/src/ImplementQueueUsingStacks.java
+++ algorithms/src/ImplementQueueUsingStacks.java
@@ -60,5 +60,34 @@ public class ImplementQueueUsingStacks {
     public boolean empty() {
         return stack.isEmpty();
     }
-
 }
+
+// more good idea..
+// https://leetcode.com/problems/implement-queue-using-stacks/discuss/64206/Short-O(1)-amortized-C%2B%2B-Java-Ruby
+// output is real data
+//
+//class MyQueue {
+//
+//    Stack<Integer> input = new Stack();
+//    Stack<Integer> output = new Stack();
+//
+//    public void push(int x) {
+//        input.push(x);
+//    }
+//
+//    public void pop() {
+//        peek();
+//        output.pop();
+//    }
+//
+//    public int peek() {
+//        if (output.empty())
+//            while (!input.empty())
+//                output.push(input.pop());
+//        return output.peek();
+//    }
+//
+//    public boolean empty() {
+//        return input.empty() && output.empty();
+//    }
+//}
