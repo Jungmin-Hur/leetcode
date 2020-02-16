679834f3d58dc40fd77cbac405e5818fbb627f61
duck <jungmin5.hur@gmail.com>
2020-02-16 17:31:21 +0900

    .

--- algorithms/src/amock/NumEquivDominoPairs.java
+++ algorithms/src/amock/NumEquivDominoPairs.java
@@ -1,7 +1,9 @@
 package amock;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
@@ -14,6 +16,20 @@ public class NumEquivDominoPairs {
     }
 
     public static int numEquivDominoPairs(int[][] dominoes) {
+        Map<Integer, Integer> dic = new HashMap<>();
+        int res = 0;
+        for(int[] domino : dominoes) {
+            int val = domino[0] < domino[1] ?
+                    domino[0] * 10 + domino[1] : domino[1] * 10 + domino[0];
+            int count = dic.getOrDefault(val,0);
+            dic.put(val, count + 1);
+            res = res + count;
+        }
+        return res;
+    }
+
+
+    public static int numEquivDominoPairs2(int[][] dominoes) {
         Map<Integer, Integer> map = new HashMap<>();
         Map<Integer, Integer> reverseMap = new HashMap<>();
 
