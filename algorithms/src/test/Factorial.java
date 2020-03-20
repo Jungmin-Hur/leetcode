6671563dffc907e178a1c111098177823cf988c7
duck <jungmin5.hur@gmail.com>
2020-03-20 20:04:44 +0900

    .

---
+++ algorithms/src/test/Factorial.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Factorial {
+
+    public static void main(String args[]) {
+        System.out.println(recursive(5));
+    }
+
+    public static int recursive(int num) {
+        if(num == 1) return 1;
+        return num * recursive(num-1);
+    }
+}
